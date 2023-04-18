45a0f6496a775f9fcd73ed82eb18334eedf5288c
SlavikKhrapach <hrapachslavik@gmail.com>
2023-04-18 12:40:25 -0400

    Set up Fat-Free for Howdy project

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+/*
+ * Slavik Khrapach
+ * 4/18/2023
+ * 328/howdy/index.php
+ * Controller for howdy project
+ *
+ */
+
+// Turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+// Require the autoload file
+require_once('vendor/autoload.php');
+
+// Create an F3 (Fat-Free Framework) object
+$f3 = Base::instance();
+
+// Define a default route
+$f3->route('GET /', function() {
+
+    echo '<h1>Wacome to My Diner!</h1>';
+
+  //echo '<h1>Hello, World!</h1>';
+    // Display view page
+    $view = new Template();
+    echo $view->render('views/home.html');
+});
+
+// Run Fat-Free
+$f3->run();
