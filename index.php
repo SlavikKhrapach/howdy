c643a921fe6b5cd58d9de653971956a1beff46db
SlavikKhrapach <hrapachslavik@gmail.com>
2023-05-11 13:16:26 -0400

    add data-layer

--- index.php
+++ index.php
@@ -13,6 +13,8 @@ error_reporting(E_ALL);
 
 // Require the autoload file
 require_once('vendor/autoload.php');
+require_once('model/data-layer.php');
+//var_dump(getMeals());
 
 // Create an F3 (Fat-Free Framework) object
 $f3 = Base::instance();
@@ -20,9 +22,6 @@ $f3 = Base::instance();
 // Define a default route
 $f3->route('GET /', function() {
 
-    //echo '<h1>Welcome to My Diner!</h1>';
-
-  //echo '<h1>Hello, World!</h1>';
     // Display view page
     $view = new Template();
     echo $view->render('views/home.html');
@@ -56,6 +55,15 @@ $f3->route('GET /dinner', function() {
     echo $view->render('views/menus/dinner.html');
 });
 
+$f3->route('GET /happy-hour', function() {
+
+    //echo '<h1>Breakfast Menu</h1>';
+
+    // Display view page
+    $view = new Template();
+    echo $view->render('views/menus/happyHour.html');
+});
+
 $f3->route('GET|POST /order1', function($f3) {
 
     // If the form has been posted
@@ -79,6 +87,11 @@ $f3->route('GET|POST /order1', function($f3) {
         // Redirect to order2 route
         $f3->reroute('order2');
     }
+
+    // Get the data from the model and add it to the hive
+    $f3->set('meals', getMeals());
+
+
     // Display view page
     $view = new Template();
     echo $view->render('views/orderForm1.html');
@@ -103,8 +116,6 @@ $f3->route('GET|POST /order2', function($f3) {
     // Display view page
     $view = new Template();
     echo $view->render('views/orderForm2.html');
-
-    session_destroy();
 });
 
 $f3->route('GET /summary', function() {
@@ -114,15 +125,8 @@ $f3->route('GET /summary', function() {
     // Display view page
     $view = new Template();
     echo $view->render('views/summary.html');
-});
 
-$f3->route('GET /happy-hour', function() {
-
-    //echo '<h1>Breakfast Menu</h1>';
-
-    // Display view page
-    $view = new Template();
-    echo $view->render('views/menus/happyHour.html');
+    session_destroy();
 });
 
 // Run Fat-Free
---
+++ model/data-layer.php
@@ -0,0 +1,12 @@
+<?php
+
+/* diner/model/data-layer.php
+    Returns data for the diner app
+    This is a part of the model
+    Eventually, this will read write the D */
+
+function getMeals() {
+    $meals = array("breakfast", "brunch", "lunch", "dinner");
+    return $meals;
+}
+
